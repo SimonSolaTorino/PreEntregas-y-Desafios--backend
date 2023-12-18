fca04c64ad3766b6be685dc84564a44b196d09e6
SimonSolaTorino <simonsolat@gmail.com>
2023-12-17 21:37:04 -0300

    agrego las autentificaciones de auth y admin, pero no las vincule a las rutas

--- src/midleware/auth.js
+++ src/midleware/auth.js
@@ -6,3 +6,11 @@ export const auth = (req, resp, next)=>{
         return resp.redirect('/now/login')
     }
 }
+export const admin = (req, resp, next)=>{
+    if(req.session?.rol === 'admin'){
+        return next()
+    }
+    else{
+        return resp.redirect('/now/login')
+    }
+}
